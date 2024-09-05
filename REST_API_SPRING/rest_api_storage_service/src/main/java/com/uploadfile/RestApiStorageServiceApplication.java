8285f3d9e6474396e00f224c961f802c2553e117
FX. Bima Yudha Pratama <bimaboim8@gmail.com>
2024-09-06 00:27:43 +0700

    insert | add project spring boot api upload file

---
+++ REST_API_SPRING/rest_api_storage_service/src/main/java/com/uploadfile/RestApiStorageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.uploadfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiStorageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiStorageServiceApplication.class, args);
+	}
+
+}
---
+++ REST_API_SPRING/rest_api_storage_service/src/test/java/com/uploadfile/rest_api_storage_service/RestApiStorageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uploadfile.rest_api_storage_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiStorageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
